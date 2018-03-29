8aa25b4a61df191c2920ab1c886d7ca45ec7c213
Jongsoo Yoon <jongsoojong@gmail.com>
2018-03-29 10:26:56 -0700

    Added header

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import './Header.css';
+
+class Header extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        }
+
+    }
+
+
+
+
+    componentDidMount() {
+    }
+
+    componentWillUnmount() {
+    }
+
+    render() {
+        return (
+            <div class="header-background">
+                <div class="header-content">
+                    <div class="logo">
+                    Jong
+                    </div>
+                    <ul class="header-links">
+                        <li class="links-props">ABOUT ME</li>
+                        <li class="links-props">PROJECTS </li>
+                        <li class="links-props">CONTACT ME</li>
+                    </ul>
+                </div>
+            </div>
+        )
+    }
+}
+export default Header;
